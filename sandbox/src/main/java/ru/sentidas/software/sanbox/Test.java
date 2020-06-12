70e7ff2a12451960bef9ca862ed17fb789977f89
Elena Bakhareva <sentidas@yandex.ru>
2020-06-12 19:42:09 +0300

    добавление функций в проект

--- sandbox/src/main/java/ru/sentidas/software/sanbox/Test.java
+++ sandbox/src/main/java/ru/sentidas/software/sanbox/Test.java
@@ -2,7 +2,31 @@ package ru.sentidas.software.sanbox;
 
 public class Test {
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+
+
+
+  double l = 5;
+    System.out.println("площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 2;
+    double b = 3;
+    System.out.println("площадь прямоугольника со сторонами " + a + " и " + b + " = " + rectangle(a,b));
+
 
   }
+
+
+
+  public static void hello (String some) {
+    System.out.println("ПРивет " + some);
+
+  }
+  public static double area (double l) {
+    return l* l;
+  }
+
+  public static double rectangle (double a, double b) {
+    return a * b;
+  }
+
 }
