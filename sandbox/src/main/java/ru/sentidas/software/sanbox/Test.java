bed854f5242240402619777d6a88e0a5ca084b03
Elena Bakhareva <sentidas@yandex.ru>
2020-06-12 17:24:24 +0300

    изменены настройки форматировавния

--- sandbox/src/main/java/ru/sentidas/software/sanbox/Test.java
+++ sandbox/src/main/java/ru/sentidas/software/sanbox/Test.java
@@ -1,8 +1,8 @@
 package ru.sentidas.software.sanbox;
 
-public class Test{
-    public static void main (String[] args){
-		System.out.println("Hello, world");
-	 
-	 }
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+
+  }
 }
