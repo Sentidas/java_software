d244380d7ea96954507d4f08ea6508f99af56913
Elena Bakhareva <sentidas@yandex.ru>
2020-06-13 20:34:46 +0300

    do task 2 (пункты 1-3)

---
+++ sandbox/src/main/java/ru/sentidas/software/sanbox/Point.java
@@ -0,0 +1,14 @@
+package ru.sentidas.software.sanbox;
+
+public class Point {
+
+  double x;
+  double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+
+}
--- sandbox/src/main/java/ru/sentidas/software/sanbox/Test.java
+++ sandbox/src/main/java/ru/sentidas/software/sanbox/Test.java
@@ -1,23 +1,19 @@
+
 package ru.sentidas.software.sanbox;
 
-import java.util.Scanner;
-
 public class Test {
+
   public static void main(String[] args) {
 
-    Square s = new Square(5);
-
-    System.out.println("площадь квадрата со стороной " + s.l + " = " + s.area());
-
-    Rectangle r = new Rectangle(2, 3 );
-
-    System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    Point p1 = new Point(5, 3);
+    Point p2 = new Point(1, 4);
+    double s = distance(p1, p2);
+    System.out.println("Расстояние между двумя точками равно " + s);
 
   }
 
-  public static void hello(String some) {
-    System.out.println("Привет " + some);
-
+  public static double distance(Point p1, Point p2) {
+    double s = Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    return s;
   }
-
 }
