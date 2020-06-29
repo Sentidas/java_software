e39d78ec9039c9c96234b02c6b003d509244b2da
Elena Bakhareva <sentidas@yandex.ru>
2020-06-29 21:03:02 +0300

    Исправления по task2 (п.4)

--- sandbox/src/main/java/ru/sentidas/software/sanbox/Test.java
+++ sandbox/src/main/java/ru/sentidas/software/sanbox/Test.java
@@ -3,13 +3,13 @@ package ru.sentidas.software.sanbox;
 
 public class Test {
 
-  public static void main(String[] args) {
+  public static  void main(String[] args) {
 
-    Point p1 = new Point(5, 3);
-    Point p2 = new Point(1, 4);
+    Point p1 = new Point(8, 3);
+    Point p2 = new Point(2, 4);
 
     System.out.println("Расстояние между двумя точками c координатами (" + p1.x + ", " + p1.y + ") " +
-            " и (" + p2.x + ", " + p2.y + ") равно " + p1.distance(p1, p2));
+            " и (" + p2.x + ", " + p2.y + ") равно " + p1.distance(p2));
   }
 }
 
