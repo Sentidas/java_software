12c19765757673fc2acfd214c2d1b6f5edbfa862
Elena Bakhareva <sentidas@yandex.ru>
2020-06-29 21:56:27 +0300

    исправления по tast2 (п. 4)
    добавлен модификатор public у метода distance

--- sandbox/src/main/java/ru/sentidas/software/sanbox/Point.java
+++ sandbox/src/main/java/ru/sentidas/software/sanbox/Point.java
@@ -9,9 +9,8 @@ public class Point {
     this.x = x;
     this.y = y;
   }
-   double distance(Point p2) {
+   public double distance(Point p2) {
     double s = Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
     return s;
   }
-
 }
