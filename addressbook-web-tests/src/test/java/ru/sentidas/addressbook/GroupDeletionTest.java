fd66567210827581154f6cff9c2d4ad3a055072d
Elena Bakhareva <sentidas@yandex.ru>
2020-06-30 19:34:35 +0300

    создание общего класса TestBase

---
+++ addressbook-web-tests/src/test/java/ru/sentidas/addressbook/GroupDeletionTest.java
@@ -0,0 +1,20 @@
+package ru.sentidas.addressbook;
+
+
+import org.testng.annotations.Test;
+
+
+public class GroupDeletionTest extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+
+    goToGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+
+  }
+
+}
+
