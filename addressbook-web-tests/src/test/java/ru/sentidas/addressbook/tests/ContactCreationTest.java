4a293cb6ab829dc2a1616b43fa28a1d4dc4aad03
Elena Bakhareva <sentidas@yandex.ru>
2020-07-03 20:50:02 +0300

    создан тест ContactCreationTest

---
+++ addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/ContactCreationTest.java
@@ -0,0 +1,17 @@
+package ru.sentidas.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.sentidas.addressbook.model.ContactData;
+
+public class ContactCreationTest extends TestBase {
+
+  @Test
+  public void testContactCreation() throws Exception {
+
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("Ivan", "Petrov", "Volgograd, Mira, 5-98", "petrov@ya.ru", "89261547865"));
+    app.getContactHelper().submitContactCreation();
+    app.getContactHelper().returnToHomePage();
+  }
+}
+
