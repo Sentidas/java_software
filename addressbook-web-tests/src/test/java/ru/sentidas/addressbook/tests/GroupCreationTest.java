a0d0ad62a12fe324c3a8970e84a0266346bb873a
Elena Bakhareva <sentidas@yandex.ru>
2020-07-05 16:59:14 +0300

    формирование коллекций, сравнение длины списков
    Лекция 4.5. Формирование коллекции

--- addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/GroupCreationTest.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.sentidas.addressbook.model.GroupData;
 
+import java.util.List;
+
 
 public class GroupCreationTest extends TestBase {
 
@@ -13,10 +15,10 @@ public class GroupCreationTest extends TestBase {
   public void testGroupCreation() throws Exception {
 
     app.getNavigationHelper().goToGroupPage();
-    int before =app.getGroupHelper().getGroupCount();
+    List<GroupData> before=app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("test3", null, null));
-    int after =app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before +1);
+    List<GroupData> after =app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() +1);
   }
 
 }
