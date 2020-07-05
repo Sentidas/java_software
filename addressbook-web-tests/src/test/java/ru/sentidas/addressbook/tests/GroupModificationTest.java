fd1e7c8bab74c8cf449b52b3efe2ef86330a1984
Elena Bakhareva <sentidas@yandex.ru>
2020-07-05 17:56:14 +0300

    сравнение коллекций в тесте GroupModificationTest
    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/GroupModificationTest.java
@@ -2,8 +2,10 @@ package ru.sentidas.addressbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.internal.GroupsHelper;
 import ru.sentidas.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest  extends TestBase {
@@ -18,11 +20,16 @@ public class GroupModificationTest  extends TestBase {
     List<GroupData> before=app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test3", "юля", null));
+    GroupData group = new GroupData(before.get(before.size()-1).getId(), "test3", "юля", null);
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after =app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() -1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<>(after));
   }
 
 
