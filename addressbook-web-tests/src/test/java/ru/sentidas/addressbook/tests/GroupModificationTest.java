ed1f7640a3666f6ecb5710cf1592151a91a449ec
Elena Bakhareva <sentidas@yandex.ru>
2020-07-13 09:39:14 +0300

    Update GroupModificationTest.java

--- addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/GroupModificationTest.java
@@ -25,15 +25,19 @@ public class GroupModificationTest  extends TestBase {
     GroupData group = new GroupData(before.get(before.size()-1).getId(), "test11", "юля", null);
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
 
     List<GroupData> after =app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
 
     before.remove(before.size() -1);
+    System.out.println(before);
     before.add(group);
+    System.out.println(before);
     Comparator<? super GroupData> byId = (g1,g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
     after.sort(byId);
+    System.out.println(after);
     Assert.assertEquals(after, before);
   }
 
