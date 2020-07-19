1b7ec5f9d252d745d8d89829cf8d0b809ee3819e
Elena Bakhareva <sentidas@yandex.ru>
2020-07-19 14:51:39 +0300

    чтение данных по контактам из xml
    Лекция 6.6. Формат XML

--- addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/GroupCreationTest.java
@@ -1,5 +1,6 @@
 package ru.sentidas.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.sentidas.addressbook.model.GroupData;
 import ru.sentidas.addressbook.model.Groups;
@@ -9,6 +10,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -18,16 +20,19 @@ public class GroupCreationTest extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
-    List<Object[]>list = new ArrayList<>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line!= null) {
+           xml +=line;
+           line = reader.readLine();
 
-           String[] split = line.split(";");
-           list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
-            line = reader.readLine();
       }
-    return  list.iterator();
+    XStream xStream = new XStream();
+    xStream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>)xStream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+
   }
 
   @Test (dataProvider = "validGroups")
