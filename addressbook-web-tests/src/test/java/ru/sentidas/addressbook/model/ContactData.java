2b4bc66f2dd1471d548a16425de069f4bc06cb1f
Elena Bakhareva <sentidas@yandex.ru>
2020-07-20 13:51:14 +0300

    реализован генератор контактов

--- addressbook-web-tests/src/test/java/ru/sentidas/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/sentidas/addressbook/model/ContactData.java
@@ -1,5 +1,6 @@
 package ru.sentidas.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -7,21 +8,30 @@ import java.util.Objects;
 
 @XStreamAlias("contact")
 public class ContactData {
-
+  @Expose
   private String firstname;
+  @Expose
   private String lastname;
+  @Expose
   private String address;
 
   @XStreamOmitField
   private int id = Integer.MAX_VALUE;
-
+  @Expose
   private String group;
+  @Expose
   private String mobilePhone;
+  @Expose
   private String homePhone;
+  @Expose
   private String workPhone;
+  @Expose
   private String allPhones;
+  @Expose
   private String email;
+  @Expose
   private String email2;
+  @Expose
   private String email3;
   private String allEmails;
 
