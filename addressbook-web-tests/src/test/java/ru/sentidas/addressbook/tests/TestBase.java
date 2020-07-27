7cc1af66b56a813f2ba68a36fd71b9367bc18970
Elena Bakhareva <sentidas@yandex.ru>
2020-07-27 13:16:55 +0300

    реазизована загрузку инфо о тестовом стенде из конфигурационного файла
    Задание №14: Вынести описание тестового стенда в конфигурационный файл
    Реализовать загрузку информации о тестовом стенде из конфигурационного файла.
    
    Цель -- избавиться от всех явных упоминаний в коде тестов адресов, имён, паролей, и прочих строковых данных, которые могут меняться при изменении конфигурации тестируемого приложения.
    
    Реализовать возможность указания при запуске тестов того, какой конфигурационный файл должен использоваться, передавая эту информацию через системные свойства.

--- addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/sentidas/addressbook/tests/TestBase.java
@@ -1,15 +1,14 @@
 package ru.sentidas.addressbook.tests;
 
-import com.applitools.eyes.selenium.BrowserType;
-import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.sentidas.addressbook.appmanager.ApplicationManager;
 
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app= new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
